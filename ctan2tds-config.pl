7bd6e7dcd8108c4fe77859c5c2e622dfb46a6e71
Norbert Preining <norbert@preining.info>
2017-07-12 11:55:47 +0900

    management scripts update

---
+++ ctan2tds-config.pl
@@ -0,0 +1,10 @@
+
+$special{'acmtrans'}    = '&MAKEflatten';
+$special{'adobecaslon'} = '&MAKEflatten';
+$special{'comicsans'}   = '&MAKEflatten';
+$special{'chemsym'}     = '&MAKEflatten';
+$special{'imprintmtshadow'} = '&donormal';
+$special{'lsabon'}      = '&donormal';
+$special{'ptmsc'}       = '&donormal';
+$special{'thsmc'}       = '&donormal';
+$special{'verdana'}     = '&donormal';

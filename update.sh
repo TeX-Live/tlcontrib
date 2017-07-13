0300e2ce502b6eef25f937744878ca26ebdedce4
Norbert Preining <norbert@preining.info>
2017-07-13 10:33:58 +0900

    update.sh uses catalogue

--- update.sh
+++ update.sh
@@ -13,26 +13,37 @@
 
 TLCHECKOUT=${TLCHECKOUT:-/home/norbert/Development/TeX/texlive.git}
 TLNETDEST=${TLNETDEST:-/home/norbert/Domains/server/texlive.info/contrib/2017}
+TLCATALOGUE=${TLCATALOGUE:-/home/norbert/Development/TeX/texcatalogue-svn}
 
 # we don't do TeX Catalogue updates
-unset TEX_CATALOGUE
+#unset TEX_CATALOGUE
 
-# update tlpdb
-$TLCHECKOUT/Master/tlpkg/bin/tl-update-tlpdb \
-	-with-w32-pattern-warning -from-git \
-	--master=`pwd`
-
-if [ "$1" = "tlpdb" ] ; then 
-  exit 0
+if [ "$1" = "container" ] ; then
+  do_container=true
+  do_tlpdb=false
+fi
+if [ "$1" = "tlpdb" ] ; then
+  do_tlpdb=true
+  do_container=false
 fi
 
-$TLCHECKOUT/Master/tlpkg/bin/tl-update-containers \
+if $do_tlpdb ; then
+  # update tlpdb
+  $TLCHECKOUT/Master/tlpkg/bin/tl-update-tlpdb \
+	-with-w32-pattern-warning -from-git \
+	--catalogue=$TLCATALOGUE	\
+	--master=`pwd`
+fi
+
+if $do_container ; then
+  $TLCHECKOUT/Master/tlpkg/bin/tl-update-containers \
 	-master `pwd` \
 	-location $TLNETDEST	\
 	-gpgcmd `pwd`/tl-sign-file \
 	-all # sometimes we need -recreate
 
-grep ^name tlpkg/texlive.tlpdb | grep -v 00texlive | grep -v '\.' | awk '{print$2}' | sort > $TLNETDEST/packages.txt
+  grep ^name tlpkg/texlive.tlpdb | grep -v 00texlive | grep -v '\.' | awk '{print$2}' | sort > $TLNETDEST/packages.txt
+fi
 
 # sometimes -recreate might be necessary to fully rebuild!
 
